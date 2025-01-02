c9c70796c8628ea7a7183e5f19600bc6e1429e9c
dushanlk <apdushan@gmail.com>
2025-01-02 12:26:38 +0530

    Scraper client web app

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export interface UrlItem {
+    url: string;
+    http_status: number;
+    error: string | null;
+  }
+  
+  export interface PaginatedUrls {
+    urls: UrlItem[];
+    next_page: string | null;
+  }
+  
+  export interface ScrapeResponse {
+    request_id: string;
+    pagination: {
+      page_size: number;
+      current_page: number;
+      total_pages: number;
+      next_page: string | null;
+    };
+    scraped: {
+      title: string;
+      html_version: string;
+      headings: {};
+      contains_login_form: boolean;
+      total_urls: number,
+      internal_urls: number,
+      external_urls: number,
+      paginated: PaginatedUrls;
+    };
+  }
+  
